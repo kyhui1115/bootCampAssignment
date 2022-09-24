d08922b6c48beb5aefcecb6a03b33d8a7179a981
민제경 <71182382+Jekyung-Min@users.noreply.github.com>
2022-09-24 09:03:50 +0900

    Update 01_bareMinimum.js

--- src/01_bareMinimum.js
+++ src/01_bareMinimum.js
@@ -190,7 +190,6 @@ _.filter = function (arr, test) {
 // _.reject는 _.filter와 정반대로 test 함수를 통과하지 않는 모든 요소를 담은 새로운 배열을 리턴합니다.
 _.reject = function (arr, test) {
   // TODO: 여기에 코드를 작성합니다.
-  // TIP: 위에서 구현한 `filter` 함수를 사용해서 `reject` 함수를 구현해 보세요.
 };
 
 // _.uniq는 주어진 배열의 요소가 중복되지 않도록 새로운 배열을 리턴합니다.
