5cd52a736bed955483919580327d6a7d90e683f8
kimyonghui <kyhui1115@gmail.com>
2022-11-21 15:01:37 +0900

    commit

--- src/Bee.js
+++ src/Bee.js
@@ -1,7 +1,13 @@
 const Grub = require('./Grub');
-
-class Bee {
-  // TODO..
+// food 속성은 grub으로부터 상속
+// eat 메소드도 상속
+class Bee extends Grub{
+  constructor() {
+    super();
+    this.age = 5;
+    this.color = 'yellow'
+    this.job = 'Keep on growing'
+  }
 }
 
 module.exports = Bee;
